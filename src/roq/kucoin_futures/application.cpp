4d22eb399d85e5896acddbf58e7dac0e5b8c7ca6
thraneh <thraneh@gmail.com>
2023-04-22 15:12:25 +0200

    api

--- src/roq/kucoin_futures/application.cpp
+++ src/roq/kucoin_futures/application.cpp
@@ -5,28 +5,33 @@
 #include "roq/kucoin_futures/config.hpp"
 #include "roq/kucoin_futures/gateway.hpp"
 
+#include "roq/kucoin_futures/flags/flags.hpp"
+
 using namespace std::literals;
 
 namespace roq {
 namespace kucoin_futures {
 
-// === CONSTANTS ===
+// === HELPERS ===
 
 namespace {
-auto const SETTINGS = server::Settings{
-    .package_name = ROQ_PACKAGE_NAME,
-    .build_number = ROQ_BUILD_NUMBER,
-    .api = {},
-    .type = server::Type::ORDER_MANAGEMENT,
+auto create_settings = []() {
+  return server::Settings{
+      .package_name = ROQ_PACKAGE_NAME,
+      .build_number = ROQ_BUILD_NUMBER,
+      .api = flags::Flags::api(),
+      .type = server::Type::ORDER_MANAGEMENT,
+  };
 };
 }
 
 // === IMPLEMENTATION ===
 
 int Application::main(int, char **) {
+  auto settings = create_settings();
   Config config;
   auto context = server::create_io_context();
-  server::Trading<Gateway>{SETTINGS, config, *context}.dispatch();
+  server::Trading<Gateway>{settings, config, *context}.dispatch();
   return EXIT_SUCCESS;
 }
 
