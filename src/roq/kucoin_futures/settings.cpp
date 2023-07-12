4ece2d19b5c361a4f0bbcd57ce9ffddbef56974f
thraneh <thraneh@gmail.com>
2023-07-12 15:12:34 +0200

    chore

--- src/roq/kucoin_futures/settings.cpp
+++ src/roq/kucoin_futures/settings.cpp
@@ -11,8 +11,8 @@ using namespace std::literals;
 namespace roq {
 namespace kucoin_futures {
 
-Settings::Settings(args::Parser const &args, server::Type type)
-    : server::flags::Settings{args, type, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, flags::Flags::api()},
+Settings::Settings(args::Parser const &args)
+    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, flags::Flags::api()},
       exchange{flags::Flags::exchange()} {
   log::debug("settings={}"sv, *this);
 }
