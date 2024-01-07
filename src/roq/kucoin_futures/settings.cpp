1a7a670fd1bead73673fde9b83d53d561eec77a9
thraneh <thraneh@gmail.com>
2024-01-07 06:17:16 +0100

    always log flags

--- src/roq/kucoin_futures/settings.cpp
+++ src/roq/kucoin_futures/settings.cpp
@@ -15,7 +15,7 @@ Settings::Settings(args::Parser const &args) : Settings{args, flags::Flags::crea
 Settings::Settings(args::Parser const &args, flags::Flags const &flags)
     : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, flags.api}, exchange{flags.exchange},
       common{flags::Common::create()}, rest{flags::REST::create()}, ws{flags::WS::create()} {
-  log::debug("settings={}"sv, *this);
+  log::info("settings={}"sv, *this);
 }
 
 }  // namespace kucoin_futures
