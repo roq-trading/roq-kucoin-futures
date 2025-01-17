dbc35aa7aa6ca9cbbdc70c911b4c85ee502dca94
thraneh <thraneh@gmail.com>
2025-01-17 09:34:06 +0100

    support Control and StatusUpdate (#484)

--- src/roq/kucoin_futures/gateway.hpp
+++ src/roq/kucoin_futures/gateway.hpp
@@ -34,6 +34,7 @@ struct Gateway final : public server::Handler, public Rest::Handler, public Orde
   void operator()(Event<Start> const &) override;
   void operator()(Event<Stop> const &) override;
   void operator()(Event<Timer> const &) override;
+  void operator()(Event<Control> const &) override;
   void operator()(Event<Connected> const &) override;
   void operator()(Event<Disconnected> const &) override;
 
