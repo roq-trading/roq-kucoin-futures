6cecee406df716173a63954fb6b60ff4dbccc46f
thraneh <thraneh@gmail.com>
2025-04-27 11:09:13 +0200

    chore

--- src/roq/kucoin_futures/drop_copy.cpp
+++ src/roq/kucoin_futures/drop_copy.cpp
@@ -6,11 +6,11 @@
 
 #include "roq/utils/update.hpp"
 
+#include "roq/utils/exceptions/unhandled.hpp"
+
 #include "roq/utils/metrics/factory.hpp"
 
 #include "roq/web/socket/client.hpp"
-
-#include "roq/core/tools/exception.hpp"
 
 #include "roq/kucoin_futures/json/utils.hpp"
 
@@ -261,7 +261,7 @@ void DropCopy::parse(std::string_view const &message) {
         log_message();
     } catch (...) {
       log_message();
-      core::tools::UnhandledException::terminate();
+      utils::exceptions::Unhandled::terminate();
     }
   });
 }
