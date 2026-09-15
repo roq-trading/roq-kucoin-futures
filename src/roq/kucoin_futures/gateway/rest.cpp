ba79cab970b4398f3301a5f97648d879dde3f59b
thraneh <thraneh@gmail.com>
2026-09-15 13:43:27 +0000

    Adding ReferenceData.market_segment and MarketSegmentStatus #636

--- src/roq/kucoin_futures/gateway/rest.cpp
+++ src/roq/kucoin_futures/gateway/rest.cpp
@@ -343,6 +343,7 @@ void Rest::operator()(Trace<protocol::json::ContractsAck> const &event) {
         .description = {},
         .security_type = security_type,
         .external_security_id = {},
+        .market_segment = {},
         .cfi_code = item.type,
         .base_currency = item.base_currency,
         .quote_currency = item.quote_currency,
